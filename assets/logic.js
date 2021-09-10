c709d8eb30ef5655494da83183023bbe5fc955b2
Spaghetti Jones <connor.breault@comcast.net>
2021-09-10 01:12:57 -0700

    contact page added

--- assets/logic.js
+++ assets/logic.js
@@ -2,6 +2,43 @@ $(document).ready(function () {
   $(".carousel").carousel();
 });
 
-$("#thing").on("click", function () {
-  console.log("clicked");
+const player = document.querySelector("lottie-player");
+player.addEventListener("complete", () => {
+  $(".contactLottie").addClass("hidden");
+  $(".contactDiv").removeClass("hidden");
+});
+
+$("#submitMessageButton").on("click", function () {
+  var messageName = $("#name").val().trim();
+  var messageEmail = $("#email").val().trim();
+  var messageMessage = $("#message").val().trim();
+
+  if (messageName === "" || messageEmail === "" || messageMessage === "") {
+    alert("Please fill out all contact information!");
+  } else {
+    $("#name").val("");
+    $("#email").val("");
+    $("#message").val("");
+    $(".contactDiv").addClass("hidden");
+    $(".loadingGif").removeClass("hidden");
+    let templateParams = {
+      name: messageName,
+      email: messageEmail,
+      message: messageMessage,
+    };
+    console.log(templateParams);
+
+    //   emailjs.send("default_service", "TEMPLATE HERE", templateParams).then(
+    //     function (response) {
+    //       console.log("Sucessful message send!");
+    //       $(".loadingGif").addClass("hidden");
+    //       $("#thanksMessage").removeClass("hidden");
+    //     },
+    //     function (error) {
+    //       console.log("FAILED...", error);
+    //       $(".loadingGif").addClass("hidden");
+    //       $("#errorMessage").removeClass("hidden");
+    //     }
+    //   );
+  }
 });
